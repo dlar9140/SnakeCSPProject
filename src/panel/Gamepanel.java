803144c055ffba7150386463baa1a2c0ba272f68
David <davidlarson46@yahoo.com>
2019-04-08 08:48:55 -0600

    imported dimension, set window measurements and made it into a dimension.

--- src/panel/Gamepanel.java
+++ src/panel/Gamepanel.java
@@ -1,15 +1,21 @@
 package panel;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Gamepanel extends JPanel{
 	
+	
+	private static final long serialVersionUID = 1L;
+	
+	public static final int WIDTH = 500, HEIGHT = 500;
+	
 
-{
+
 	public Gamepanel() {
 		
-		
-		
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 	}
 
 }
