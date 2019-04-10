320e72df1f988d28ea559eabedc80ab36df34866
David <davidlarson46@yahoo.com>
2019-04-10 07:59:30 -0600

    made if loops to make snake bigger, made an arraylist for the snake model.

--- src/panel/Gamepanel.java
+++ src/panel/Gamepanel.java
@@ -2,9 +2,12 @@ package panel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import Model.Snakemodel;
 
 public class Gamepanel extends JPanel implements Runnable{
 	
@@ -17,11 +20,21 @@ public class Gamepanel extends JPanel implements Runnable{
 	
 	private boolean running;
 	
-
+	private boolean right = true, left = false, up = false, down = false;
+	
+	private Snakemodel b;
+	private ArrayList<Snakemodel> snake;
+	
+	private int xCoor = 10, yCoor = 10, size = 5;
+	private int ticks = 0;
 
 	public Gamepanel() {
 		
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+		snake = new ArrayList<Snakemodel>();
+		
+		start();
 	}
 	
 	public void start() 
@@ -46,6 +59,12 @@ public class Gamepanel extends JPanel implements Runnable{
 	}
 	public void tick() 
 	{
+		if(snake.size() == 0)
+		{
+			b = new Snakemodel(xCoor, yCoor, 10);
+			snake.add(b);
+		}
+		ticks++;
 		
 	}
 	public void paint(Graphics g) 
@@ -63,6 +82,10 @@ public class Gamepanel extends JPanel implements Runnable{
 		for(int i = 0 ; i < HEIGHT/10 ; i++)
 		{
 			g.drawLine(0, i * 10, HEIGHT, i * 10);
+		}
+		for(int i = 0 ; i < snake.size() ; i++)
+		{
+			snake.get(i).draw(g);
 		}
 	}
 
