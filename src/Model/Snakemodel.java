1e1e6e85339c0a2d61611a620609fbf9f2917f69
David <davidlarson46@yahoo.com>
2019-04-10 07:48:02 -0600

    started on the snake model, having it fill the model, made getters and setters.

--- src/Model/Snakemodel.java
+++ src/Model/Snakemodel.java
@@ -1,6 +1,48 @@
 package Model;
+import java.awt.Color;
+import java.awt.Graphics;
+
 
 public class Snakemodel
 {
+	private int xCoor, yCoor, width, height;
+	
+	public Snakemodel(int xCoor, int yCoor, int titleSize)
+	{
+		this.xCoor = xCoor;
+		this.yCoor = yCoor;
+		width = titleSize;
+		height = titleSize;
+	}
+	
+	public void tick()
+	{
+		
+	}
+		public void draw(Graphics g)
+		{
+			g.setColor(Color.YELLOW);
+			g.fillRect(xCoor * width, yCoor * height, width, height);
+			
+		}
 
+		public int getxCoor()
+		{
+			return xCoor;
+		}
+
+		public void setxCoor(int xCoor)
+		{
+			this.xCoor = xCoor;
+		}
+
+		public int getyCoor()
+		{
+			return yCoor;
+		}
+
+		public void setyCoor(int yCoor)
+		{
+			this.yCoor = yCoor;
+		}
 }
