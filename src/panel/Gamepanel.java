f1a1701f26b4b6384937e7b2e443f6ff2aa83104
David <davidlarson46@yahoo.com>
2019-04-11 07:54:00 -0600

    setting up controls and snake movement.

--- src/panel/Gamepanel.java
+++ src/panel/Gamepanel.java
@@ -2,6 +2,8 @@ package panel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -9,7 +11,8 @@ import javax.swing.JPanel;
 
 import Model.Snakemodel;
 
-public class Gamepanel extends JPanel implements Runnable{
+public class Gamepanel extends JPanel implements Runnable, KeyListener
+{
 	
 	
 	private static final long serialVersionUID = 1L;
@@ -113,6 +116,53 @@ public class Gamepanel extends JPanel implements Runnable{
 			tick();
 			repaint();
 		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e)
+	{
+		int key = e.getKeyCode();
+		if(key == KeyEvent.VK_RIGHT && !left)
+		{
+			right = true;
+			up = false;
+			down = false;
+		}
+		
+		if(key == KeyEvent.VK_LEFT && !right)
+		{
+			left = true;
+			 up = false;
+			down = false;
+		}
+		
+		if(key == KeyEvent.VK_UP && !down)
+		{
+			up = true;
+			left = false;
+			right = false;
+		}
+		
+		if(key == KeyEvent.VK_DOWN && !up)
+		{
+			left = true;
+			up = false;
+			down = false;
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+		
+		
 	}
 
 }
